019d75b8f5bb9343a02fde7d47efaa44d015a770
Devin Torres <devin@devintorr.es>
2016-02-03 23:09:11 -0600

    Allow async PostCSS plugins
    Closes #2.

--- test/test.js
+++ test/test.js
@@ -3,11 +3,11 @@ var posthtml = require('posthtml');
 var css = require('..');
 var expect = require('chai').expect;
 
-function test(html, referense, postcssOptions, done) {
+function test(html, expected, postcssOptions, done) {
     expect(posthtml([css([require('autoprefixer')({ browsers: ['last 2 versions'] })], postcssOptions)])
         .process(html)
         .then(function(result) {
-            expect(referense).to.eql(result.html);
+            expect(expected).to.eql(result.html);
             done();
         }).catch(function(error) {
             done(error);
@@ -15,35 +15,37 @@ function test(html, referense, postcssOptions, done) {
 }
 
 describe('use postcss', function() {
+    it('options', function() {
+        expect(function() { posthtml([css([])]); }).to.not.throw(Error);
+    });
 
     it('style tag', function(done) {
         var html = '<style>a {display: flex;}</style>';
-        var referense = '<style>a {display: -webkit-flex;display: -ms-flexbox;display: flex;}</style>';
-        test(html, referense, {}, done);
+        var expected = '<style>a {display: -webkit-flex;display: -ms-flexbox;display: flex;}</style>';
+        test(html, expected, {}, done);
     });
 
     it('style tag empty', function(done) {
         var html = '<style></style>';
-        var referense = '<style></style>';
-        test(html, referense, {}, done);
+        var expected = '<style></style>';
+        test(html, expected, {}, done);
     });
 
     it('style attrs', function(done) {
         var html = '<div style="display: flex;"></div>';
-        var referense = '<div style="display: -webkit-flex;display: -ms-flexbox;display: flex;"></div>';
-        test(html, referense, {}, done);
+        var expected = '<div style="display: -webkit-flex;display: -ms-flexbox;display: flex;"></div>';
+        test(html, expected, {}, done);
     });
 
     it('style attrs empty', function(done) {
         var html = '<div style></div>';
-        var referense = '<div style></div>';
-        test(html, referense, {}, done);
+        var expected = '<div style=""></div>';
+        test(html, expected, {}, done);
     });
 
     it('no style', function(done) {
         var html = 'text <div></div>';
-        var referense = 'text <div></div>';
-        test(html, referense, {}, done);
+        var expected = 'text <div></div>';
+        test(html, expected, {}, done);
     });
 });
-
